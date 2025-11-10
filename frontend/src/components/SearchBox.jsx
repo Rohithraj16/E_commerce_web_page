cc721696731584aa116a0183064cc0678233595f
Rohithraj16 <www.rohithraj08@gmail.com>
2025-11-10 13:51:02 +0530

    frontend and backend completed

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -1,52 +1,52 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
-	Form,
-	Button,
-	InputGroup,
-	OverlayTrigger,
-	Tooltip,
-} from "react-bootstrap";
-import { FaSearch, FaTimes } from "react-icons/fa";
-import { useDispatch } from "react-redux";
-import { searchProduct, clearSearch } from "../slices/searchProductSlice";
+  Form,
+  Button,
+  InputGroup,
+  OverlayTrigger,
+  Tooltip
+} from 'react-bootstrap';
+import { FaSearch, FaTimes } from 'react-icons/fa';
+import { useDispatch } from 'react-redux';
+import { searchProduct, clearSearch } from '../slices/searchProductSlice';
 
 function SearchBox() {
-	const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
 
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const searchProductHandler = (e) => {
-		e.preventDefault();
-		dispatch(searchProduct(input));
-	};
+  const searchProductHandler = e => {
+    e.preventDefault();
+    dispatch(searchProduct(input));
+  };
 
-	const clearSearchHandler = () => {
-		dispatch(clearSearch());
-		setInput("");
-	};
-	return (
-		<Form onSubmit={searchProductHandler} className="d-flex">
-			<InputGroup>
-				<Form.Control
-					size="sm"
-					type="text"
-					value={input}
-					onChange={(e) => setInput(e.target.value)}
-					placeholder="Search Products..."
-				/>
-				{input === "" ? (
-					""
-				) : (
-					<Button type="button" variant="light" onClick={clearSearchHandler}>
-						<FaTimes />
-					</Button>
-				)}
-				<Button type="submit" variant="warning">
-					<FaSearch />
-				</Button>
-			</InputGroup>
-		</Form>
-	);
+  const clearSearchHandler = () => {
+    dispatch(clearSearch());
+    setInput('');
+  };
+  return (
+    <Form onSubmit={searchProductHandler} className='d-flex'>
+      <InputGroup>
+        <Form.Control
+          size='sm'
+          type='text'
+          value={input}
+          onChange={e => setInput(e.target.value)}
+          placeholder='Search Products...'
+        />
+        {input === '' ? (
+          ''
+        ) : (
+          <Button type='button' variant='light' onClick={clearSearchHandler}>
+            <FaTimes />
+          </Button>
+        )}
+        <Button type='submit' variant='warning'>
+          <FaSearch />
+        </Button>
+      </InputGroup>
+    </Form>
+  );
 }
 
 export default SearchBox;
